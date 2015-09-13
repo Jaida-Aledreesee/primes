c862556b1d48889a007d631b2c369c31479d356c
Jaida-Aledreesee <jaledreesee@Jaydaas-MBP.att.net>
2015-09-13 18:59:00 -0500

    primes

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class checknumbers
+{
+
+    public Boolean checkNumber(int number)
+    {
+       
+
+            int i;
+            if (number == 1)
+            {
+                return false;
+
+            }
+            else if (number == 2)
+            {
+                return true;
+            }
+            else if (number % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+
+                for (i = 3; i <= Math.Sqrt(number) + 1; i = i + 2)
+                {
+
+                    if (number % i == 0)
+                    {
+
+                        return false;
+                     
+                    }
+                }
+
+                if (i >= Math.Sqrt(number) + 1)
+                {
+
+                    return true;
+
+                }
+               
+                return true;
+               
+            }
+          
+        }
+
+
+    }
+
+
+    
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeNumbers
+{
+    class Primes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if it is a prime or not: ");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            checknumbers check = new checknumbers();
+            if (check.checkNumber(number))
+            {
+                Console.WriteLine("It is prime");
+            }
+            else
+            {
+                Console.WriteLine("It is not prime");
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
